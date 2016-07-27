76b74120734128c8a07c06424176222cb3df4634
Rachel <rachel.black@flashtalking.com>
2016-07-27 16:42:37 +0100

    query string parser added

--- router.js
+++ router.js
@@ -1,14 +1,16 @@
 const Authentication = require('./controllers/authentication')
-const clientSecret = require('./config').clientSecret
-const liRedirectURL = require('./config').liRedirecURL
-const liStateString = require('./config').liStateString
-const clientId = require('./config').clientId
+const configs = require('./config')
+const urlParse = require('./utils/query_string_parser')
 //TODO create a controller for githubapi calls https://www.udemy.com/react-redux-tutorial/learn/v4/t/lecture/4755164
 
 module.exports = function(app){
   app.get('/lisignup', function (req, res) {
       res.redirect(301,
-          'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id='+ clientId + '&redirect_uri='+ liRedirectURL +'&state=' + liStateString + '=r_emailaddress')
+          'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id='+ configs.clientId + '&redirect_uri='+ configs.liRedirectURL +'&state=' + configs.liStateString + '=r_emailaddress')
+  })
+
+  app.get(/lisignup2/, function(req, res){
+      res.send(urlParse('code',req.url))
   })
   app.post('/signup', Authentication.signup)
 }
