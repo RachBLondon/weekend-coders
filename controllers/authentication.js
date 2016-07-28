b1cb4b362e728634b52a4d2e4b8d1b6ade94243a
Rachel <rachel.black@flashtalking.com>
2016-07-28 17:24:46 +0100

    refactoring auth logic to controllers/auth

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,9 +1,58 @@
+const configs = require('./../config')
+const urlParse = require('./../utils/query_string_parser')
+const https = require('https')
+const request = require('request')
+//TODO create a controller for githubapi calls https://www.udemy.com/react-redux-tutorial/learn/v4/t/lecture/4755164
+const hostUrl = 'http://localhost:3090/'
+
 exports.signup = function(req, res, next){
-    //Check if user with email already exisists
+        res.redirect(302,
+            'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=' + configs.clientId + '&redirect_uri=' + configs.liRedirectURL + '&state=' + configs.liStateString )
+}
 
-    // if users with email does exist return an error
+exports.signupSuccess = function(req, res){
+    const authorizationCode = urlParse('code', req.url)
+    const postBody = 'grant_type=authorization_code&code=' + authorizationCode + '&state=' + configs.liStateString + '&redirect_uri=' + configs.liRedirectURL + '&client_id=' + configs.clientId + '&client_secret=' + configs.clientSecret
+    console.log('postBody :', postBody)
 
-    // if user does not exist create and save a user record
+    var accessTokenPostOptions = {
+        hostname: 'www.linkedin.com',
+        path: '/oauth/v2/accessToken',
+        method: 'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        agent: false
+    };
 
-    // respond to say user login was created
+    var postReq = https.request(accessTokenPostOptions, (postRes) => {
+        var body = '';
+        postRes.on('data', function (chunk) {
+            body += chunk
+        })
+        postRes.on('end', function () {
+            var accessToken = JSON.parse(body).access_token
+
+            var userDetails = {
+                hostname: 'www.linkedin.com',
+                path: '/v1/people/~:(id,first-name,last-name,location,email-address,picture-url,num-connections,positions)?&format=json',
+                method: 'GET',
+                headers: {'Authorization': 'Bearer ' + accessToken},
+                agent: false
+            }
+
+            var getUserData = https.request(userDetails, (dataRes)=> {
+                var getResponseBody = ''
+                dataRes.on('data', function (chunk) {
+                    getResponseBody += chunk
+                })
+                dataRes.on('end', function () {
+                    console.log("userData :", JSON.parse(getResponseBody))
+                    res.redirect(302, hostUrl + 'account/'+ accessToken)
+                })
+            })
+
+            getUserData.end()
+        })
+    });
+    postReq.write(postBody)
+    postReq.end()
 }
--- router.js
+++ router.js
@@ -1,64 +1,11 @@
 const Authentication = require('./controllers/authentication')
 const configs = require('./config')
-const urlParse = require('./utils/query_string_parser')
-const https = require('https')
-const request = require('request')
-//TODO create a controller for githubapi calls https://www.udemy.com/react-redux-tutorial/learn/v4/t/lecture/4755164
-const hostUrl = 'http://localhost:3090/'
+
+
+
 module.exports = function (app) {
-    app.get('/signup', function (req, res) {
-        res.redirect(302,
-            'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=' + configs.clientId + '&redirect_uri=' + configs.liRedirectURL + '&state=' + configs.liStateString )
-    })
-
-    app.get(/signup_success/, function (req, res) {
-        const authorizationCode = urlParse('code', req.url)
-
-        const postBody = 'grant_type=authorization_code&code=' + authorizationCode + '&state=' + configs.liStateString + '&redirect_uri=' + configs.liRedirectURL + '&client_id=' + configs.clientId + '&client_secret=' + configs.clientSecret
-        console.log('postBody :', postBody)
-
-        var accessTokenPostOptions = {
-            hostname: 'www.linkedin.com',
-            path: '/oauth/v2/accessToken',
-            method: 'POST',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            agent: false
-        };
-
-        var postReq = https.request(accessTokenPostOptions, (postRes) => {
-            var body = '';
-            postRes.on('data', function (chunk) {
-                body += chunk
-            })
-            postRes.on('end', function () {
-                var accessToken = JSON.parse(body).access_token
-
-                var userDetails = {
-                    hostname: 'www.linkedin.com',
-                    path: '/v1/people/~:(id,first-name,last-name,location,email-address,picture-url,num-connections,positions)?&format=json',
-                    method: 'GET',
-                    headers: {'Authorization': 'Bearer ' + accessToken},
-                    agent: false
-                }
-
-                var getUserData = https.request(userDetails, (dataRes)=> {
-                    var getResponseBody = ''
-                    dataRes.on('data', function (chunk) {
-                        getResponseBody += chunk
-                    })
-                    dataRes.on('end', function () {
-                        console.log("userData :", JSON.parse(getResponseBody))
-                        res.redirect(302, hostUrl + 'account/'+ accessToken)
-                    })
-                })
-
-                getUserData.end()
-            })
-        });
-        postReq.write(postBody)
-        postReq.end()
-
-    })
+    app.get('/signup', Authentication.signup)
+    app.get(/signup_success/, Authentication.signupSuccess)
     app.get('/account/:code', function (req, res) {
         res.send(req.params.code)
     })
