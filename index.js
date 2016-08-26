be8b4f86753f2d9572bb1ddf1f8680ea4cf2ea16
Rachel <rachel.black@flashtalking.com>
2016-08-26 15:14:30 +0100

    scss setup

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(dbURI)
 
 //App Setup
 //middleware
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*'}))
 app.use(cookieParser())
 app.use('/public', express.static(__dirname + '/public'))
