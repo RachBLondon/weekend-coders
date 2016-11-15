afaace5a81ac0f3ea4bff87b9489c49db0efc8f9
Rachel <rachel.black@flashtalking.com>
2016-11-15 18:25:25 +0000

    removing github search from server

--- router.js
+++ router.js
@@ -1,25 +1,21 @@
 const Authentication = require('./controllers/authentication')
-const Github = require('./controllers/github')
 const Profile = require('./controllers/profile')
-
 const User = require('./models/user')
-
-
+const path = require('path')
 
 module.exports = function (app) {
-    app.get('/signup', Authentication.signup)
-    app.get(/signup_success/, Authentication.signupSuccess)
+    app.get('/signup', Authentication.signup);
+    app.get(/signup_success/, Authentication.signupSuccess);
     app.get('/', function(req, res){
-        res.sendFile(__dirname + '/public/index.html')
-    })
+        res.sendFile(__dirname + '/public/index.html');
+    });
     app.get('/logout', Authentication.logout, function(req, res){
         res.sendFile('logged out')
-    })
-    app.get('/github/search', Github.searchGithub)
-    app.get('/github/pagination', Github.pagination)
+    });
     app.get('/getprofile', Profile.getProfile)
-    app.get('/getshortlist', Github.getShortList)
-    app.post('/addToShortList', Github.addToShortList)
-    app.get('*', Authentication.isAuthenticated, Github.gitHubApp )
+    app.get('*', Authentication.isAuthenticated, function (req, response) {
+            response.sendFile(path.join(__dirname, '/client/index.html'))
+        }
+    );
 }
 
