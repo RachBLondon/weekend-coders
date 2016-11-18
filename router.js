ba765af7978695b4b3b3a23e8c0f211767bba5a5
Rachel <rachel.black@flashtalking.com>
2016-11-18 16:22:47 +0000

    protecting getprojects route and renaming addnewproject

--- router.js
+++ router.js
@@ -15,9 +15,9 @@ module.exports = function (app) {
     app.get('/logout', Authentication.logout, function(req, res){
         res.sendFile('logged out')
     });
-    app.post('/test', Projects.createNewProject);
+    app.post('/addnewproject', Projects.createNewProject);
     app.get('/getprofile', Profile.getProfile);
-    app.get('/getprojects', Projects.getAll);
+    app.get('/getprojects',Authentication.isAuthenticated, Projects.getAll);
     app.get('*', Authentication.isAuthenticated, function (req, response) {
             response.sendFile(path.join(__dirname, '/client/index.html'));
         }
